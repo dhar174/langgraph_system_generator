96e25bec3caa0adc04e91b0d7cd64ae4015b8571
Charles I Niswander II <darf333@gmail.com>
2026-01-13 12:03:18 -0500

    Bump version from 0.1.0 to 0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (BASE_DIR / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="langgraph-system-generator",
-    version="0.1.0",
+    version="0.1.1",
     description="LangGraph Notebook Foundry scaffolding",
     long_description=README,
     long_description_content_type="text/markdown",
